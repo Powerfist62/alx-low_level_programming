ed87937561fa1de3debd9fbd0eef1715762f9143
powerfist62 <powerfist62@gmail.com>
2022-10-15 14:19:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success0
+  */
+
+int main(void)
+{
+	char Low;
+
+	for (Low = 'a'; Low <= 'z'; Low++)
+		putchar(Low);
+		putchar('\n');
+
+	return (0);
+
+}
