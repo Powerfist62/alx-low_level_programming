21d522c89f20fed206b9bb3e7048697c8d18190b
powerfist62 <powerfist62@gmail.com>
2022-10-18 04:00:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - putchar function
+  * Description: Get the character to be wrtten
+  * Return: 0.
+  */
+int main(void)
+{
+
+	char ch = 'G';
+
+	putchar(ch);
+
+	return (0);
+}
